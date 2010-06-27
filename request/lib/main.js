6fb4e19eb5900a080fe24e205d8489cc5661da01
Robert McGuire <robert.m.mcguire@gmail.com>
2010-06-26 20:52:25 -0400

    Add support for options.timeout

--- request/lib/main.js
+++ request/lib/main.js
@@ -11,10 +11,10 @@ function request (options, callback) {
       options.uri = url.parse(options.uri);
     }
   }
-  
+
   options.followRedirect = options.followRedirect ? options.followRedirect : true;
   options.method = options.method ? options.method : 'GET';
-  
+
   options.headers = options.headers ? options.headers :  {};
   if (!options.headers.host) {
     options.headers.host = options.uri.hostname;
@@ -25,40 +25,50 @@ function request (options, callback) {
   } else {
     var setHost = false;
   }
-  
+
   if (!options.uri.pathname) {options.uri.pathname = '/'}
   if (!options.uri.port) {
     if (options.uri.protocol == 'http:') {options.uri.port = 80}
     else if (options.uri.protocol == 'https:') {options.uri.port = 443}
   }
-  
+
   if (options.uri.protocol == 'https:') {
-    var secure = true; 
+    var secure = true;
   } else {
     var secure = false;
   }
-  
+
   if (options.bodyStream) {
     sys.error('options.bodyStream is deprecated. use options.reponseBodyStream instead.');
     options.responseBodyStream = options.bodyStream;
   }
-  
+
   options.client = options.client ? options.client : http.createClient(options.uri.port, options.uri.hostname, secure);
-  
+
   var clientErrorHandler = function (error) {
     if (setHost) delete options.headers.host;
     callback(error);
   }
   options.client.addListener('error', clientErrorHandler);
-  
+
+  if (options.timeout) {
+    options.client.setTimeout(options.timeout);
+    options.client.addListener('timeout', function() {
+      options.client.destroy();
+      clientErrorHandler({message: "Timeout exceeded (" + options.timeout
+                                   + ")."});
+    });
+  }
+
+
   if (options.uri.auth && !options.headers.authorization) {
     options.headers.authorization = "Basic " + base64.encode(options.uri.auth);
   }
   options.fullpath = options.uri.href.replace(options.uri.protocol + '//' + options.uri.host, '');
-  if (options.fullpath.length === 0) options.fullpath = '/' 
+  if (options.fullpath.length === 0) options.fullpath = '/'
   if (options.body) {options.headers['content-length'] = options.body.length}
   options.request = options.client.request(options.method, options.fullpath, options.headers);
-  
+
   options.request.addListener("response", function (response) {
     var buffer = '';
     response.addListener("data", function (chunk) {
@@ -68,17 +78,17 @@ function request (options, callback) {
     })
     response.addListener("end", function () {
       options.client.removeListener("error", clientErrorHandler);
-      
+
       if (response.statusCode > 299 && response.statusCode < 400 && options.followRedirect && response.headers.location) {
         options.uri = response.headers.location;
-        delete options.client; 
+        delete options.client;
         if (options.headers) {
           delete options.headers.host;
         }
         request(options, callback);
         return;
       }
-      
+
       if (options.responseBodyStream) {
         buffer = options.responseBodyStream;
       }
@@ -86,7 +96,7 @@ function request (options, callback) {
       callback(null, response, buffer);
     })
   })
-  
+
   if (options.body) {
     options.request.write(options.body, 'binary');
     options.request.end();
